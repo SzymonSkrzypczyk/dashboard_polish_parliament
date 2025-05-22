8a720dde9fa23c304bd89e16ab4322202381142a
Szymon Skrzypczyk <77851583+SzymonSkrzypczyk@users.noreply.github.com>
2025-05-22 17:20:44 +0200

    more functions for retrieving data

---
+++ fetch_data.py
@@ -0,0 +1,84 @@
+from dataclasses import dataclass
+from string import Template
+import requests
+
+BASE_URL = Template("https://api.sejm.gov.pl/sejm/term$term")
+
+
+@dataclass
+class MP:
+    first_name: str
+    last_name: str
+    active: bool
+    educationLevel: str
+    profession: str
+    voivodeship: str
+    numberOfVotes: int
+
+
+def get_clubs(term: int = 10):
+    response = requests.get(f"{BASE_URL.substitute(term=term)}/clubs")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return []
+
+
+def get_all_by_term(term: int = 10):
+    response = requests.get(f"{BASE_URL.substitute(term=term)}/MP")
+    if response.status_code == 200:
+        members = response.json()
+        # educationLevel, active, profession, voivodeship
+        return members
+    else:
+        return []
+
+
+def get_all_interpellations(term: int = 10):
+    response = requests.get(f"{BASE_URL.substitute(term=term)}/interpellations")
+    if response.status_code == 200:
+        interpellations = response.json()
+        return interpellations
+    else:
+        return []
+
+
+def get_all_prints(term: int = 10):
+    response = requests.get(f"{BASE_URL.substitute(term=term)}/prints")
+    if response.status_code == 200:
+        prints = response.json()
+        return prints
+    else:
+        return []
+
+
+def get_all_proceedings(term: int = 10):
+    response = requests.get(f"{BASE_URL.substitute(term=term)}/proceedings")
+    if response.status_code == 200:
+        proceedings = response.json()
+        return proceedings
+    else:
+        return []
+
+
+def get_votings_per_day(term: int = 10):
+    response = requests.get(f"{BASE_URL.substitute(term=term)}/proceedings")
+    if response.status_code == 200:
+        proceedings = response.json()
+        return proceedings
+    else:
+        return []
+
+
+def get_members_by_club(club_id: str, term:int = 10):
+    response = requests.get(f"{BASE_URL.substitute(term=term)}/MP")
+    if response.status_code == 200:
+        members = response.json()
+        # educationLevel, active, profession, voivodeship
+        return [member for member in members if member.get('club') == club_id]
+    else:
+        return []
+
+
+if __name__ == '__main__':
+    print(get_members_by_club("KO"))
--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from fetch_data import get_clubs, get_members_by_club
 
-st.title("dupa")
+st.title("Dashboard Sejmu RP")
 
-with st.sidebar:
-    st.header("Settings")
+clubs = get_clubs()
+club_names = [club['name'] for club in clubs]
 
-    party = st.selectbox("Party", ["P1", "P2", "P3", "P4", "P5"])
+selected_clubs = st.sidebar.multiselect("Wybierz partie", club_names)
+
+selected_term = st.sidebar.slider("Wybierz kadencje", 1, 10, 10)
+
+if selected_clubs:
+    # for selected_club in selected_clubs:
+    #     club_id = next((club['id'] for club in clubs if club['name'] == selected_club), None)
+    col1, col2, col3 = st.columns(3, gap="medium", vertical_alignment="center")
+
+    with col1:
+        # liczba aktywnych poslow
+        # rozklad procentowy poslow na kluby
+        st.text("1")
+
+    with col2:
+        # wyksztalcenie poslow
+        # wojewodztwa z ktorych sa
+        st.text("2")
+
+    with col3:
+        # liczba posiedzen
+        # liczba interapelacji
+        st.text("3")
+
+
+else:
+    st.info("Proszę wybrać co najmniej jedną partię z paska bocznego.")
